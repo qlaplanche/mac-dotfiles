37fbea572fc8775278945db41c00c5e7ba08c571
Quentin Laplanche <quentin.laplanche@outlook.fr>
2017-02-23 16:19:51 +0100

    script install and brewfile

---
+++ scripts/install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+echo "Setting up your Mac..."
+
+# Check for Homebrew and install if we don't have it
+if test ! $(which brew); then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update Homebrew recipes
+brew update
+
+# Install all our dependencies with bundle (See Brewfile)
+brew tap homebrew/bundle
+brew bundle
+
+# brew install mackup
+# mackup restore
+
+# Create a Sites directory
+# This is a default directory for macOS user accounts but doesn't comes pre-installed
+mkdir $HOME/Sites
+
+# Set macOS preferences
+# We will run this last because this will reload the shell
+source macos
+
+
